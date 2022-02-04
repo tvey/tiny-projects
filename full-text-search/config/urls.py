3e7989120b2b2d47c769d108ee5aa39dd3e89763
tvey <liteitt@gmail.com>
2022-02-04 19:16:59 +0300

    FTS cleanup

--- full-text-search/config/urls.py
+++ full-text-search/config/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from quotes.views import IndexView, search, QuotesEn, QuotesRu
+from quotes.views import IndexView, search, QuotesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
-    path('en/', QuotesEn.as_view(), name='quotes-en'),
-    path('ru/', QuotesRu.as_view(), name='quotes-ru'),
-    path('en/search/', search, name='search'),
-    path('ru/search/', search, name='search'),
+    path('en/', QuotesView.as_view(), name='quotes-en'),
+    path('ru/', QuotesView.as_view(), name='quotes-ru'),
+    path('en/search/', search, name='search-en'),
+    path('ru/search/', search, name='search-ru'),
 ]
--- full-text-search/quotes/views.py
+++ full-text-search/quotes/views.py
@@ -15,26 +15,15 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class QuotesEn(TemplateView):
-    template_name = 'quotes_en.html'
-
-
-class QuotesRu(TemplateView):
-    template_name = 'quotes_ru.html'
-
-
-def search_util(lang):
-    if lang == 'en':
-        pass
-    elif lang == 'ru':
-        pass
+class QuotesView(TemplateView):
+    template_name = 'quotes.html'
 
 
 def search(request):
     q = request.GET.get('q')
 
     if q:
-        vector = SearchVector('content')
+        vector = SearchVector('content', 'author__name_en', 'author__name_ru')
         query = SearchQuery(q)
         rank = SearchRank(vector, query)
         search_headline = SearchHeadline('content', query, highlight_all=True)
@@ -44,9 +33,7 @@ def search(request):
             .order_by('-rank')
             .filter(rank__gt=0)
         )
-        print(quotes)
         result = quotes.values('headline', 'author__name_en', 'author__name_ru')
-        # print(list(result))
         return JsonResponse(list(result), safe=False)
 
     return JsonResponse([])
