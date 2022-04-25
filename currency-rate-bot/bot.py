6b8b9d53e963b7d529b1e1683ce2d7ecddf744db
tvey <liteitt@gmail.com>
2022-04-25 22:22:11 +0300

    Currency calculator

--- currency-rate-bot/bot.py
+++ currency-rate-bot/bot.py
@@ -2,7 +2,9 @@ import os
 
 import dotenv
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from utils import get_rates, CURRENCIES
 
@@ -13,14 +15,25 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
-CURRENCY_CODES = [c['code'] for c in CURRENCIES]
+
+calc_directions = ['Из валюты в рубли', 'Из рублей в валюту']
+calc_currencies = ['Доллар', 'Евро', 'Юань']
+currency_codes = ['USD', 'EUR', 'CNY']
+
+
+class CalculatorStates(StatesGroup):
+    direction_state = State()
+    currency_state = State()
+    amount_state = State()
 
 
 @dp.message_handler(commands=['start'])
 async def reply_on_start(message: types.Message):
     text = 'Актуальные курсы валют ЦБ РФ.'
-    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    kb.add(*['$', '€', 'Все валюты'])
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.row(*['$', '€'])
+    kb.row('Все валюты')
+    kb.row('Калькулятор')
     await message.reply(text, reply_markup=kb)
 
 
@@ -30,9 +43,9 @@ async def display_all_currencies(message: types.Message):
     kb = types.InlineKeyboardMarkup(row_width=1)
     currency_buttons = [
         types.InlineKeyboardButton(
-            text=f'{c["nominal"]} {c["name"]}', callback_data=c['code']
+            text=f'{v["nominal"]} {v["name"]}', callback_data=k
         )
-        for c in CURRENCIES
+        for k, v in CURRENCIES.items()
     ]
     kb.add(*currency_buttons)
     await message.answer(text, reply_markup=kb)
@@ -49,6 +62,59 @@ async def get_rate(callback: types.CallbackQuery):
     rate_data = await get_rates()
     await callback.message.answer(rate_data[callback.data])
     await callback.answer()
+
+
+@dp.message_handler(commands=['Калькулятор'])
+async def start_calculator(message: types.Message):
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(*calc_directions)
+    await message.answer('Как посчитать?', reply_markup=kb)
+    await CalculatorStates.direction_state.set()
+
+
+async def direction_currency(message: types.Message, state: FSMContext):
+    if message.text not in calc_directions:
+        text = 'Пожалуйста, выберите направление, используя клавиатуру ниже.'
+        await message.answer(text)
+        return
+    if message.text == calc_currencies[0]:
+        await state.update_data(direction=0)
+    else:
+        await state.update_data(direction=1)
+
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(*calc_currencies)
+
+    await CalculatorStates.next()
+    await message.answer('Выберите валюту:', reply_markup=kb)
+
+
+async def currency_amount(message: types.Message, state: FSMContext):
+    if message.text not in calc_currencies:
+        text = 'Выберите валюту из тех, что представлены ниже.'
+        await message.answer(text)
+        return
+    currency_index = calc_currencies.index(message.text)
+    await state.update_data(direction=currency_codes[currency_index])
+
+    await CalculatorStates.next()
+    await message.answer('Введите сумму:')
+
+
+async def result(message: types.Message, state: FSMContext):
+    amount = 0
+    try:
+        amount = int(message.text)
+    except ValueError:
+        await message.answer('Введите положительное число, пожалуйста.')  #
+        return
+    await state.update_data(amount=amount)
+
+    user_state_data = await state.get_data()
+
+    # calculate
+    await message.answer()
+    await state.finish()
 
 
 if __name__ == '__main__':
--- currency-rate-bot/utils.py
+++ currency-rate-bot/utils.py
@@ -12,170 +12,137 @@ async def get_rates():
     return rates | {'$': rates['USD'], '€': rates['EUR']}
 
 
-CURRENCIES = [
-    {
-        'code': 'AUD',
-        'nominal': '1',
-        'name': 'австралийский доллар',
+CURRENCIES = {
+    "AUD": {
+        "nominal": "1",
+        "name": "австралийский доллар",
     },
-    {
-        'code': 'AZN',
-        'nominal': '1',
-        'name': 'азербайджанский манат',
+    "AZN": {
+        "nominal": "1",
+        "name": "азербайджанский манат",
     },
-    {
-        'code': 'GBP',
-        'nominal': '1',
-        'name': 'фунт стерлингов',
+    "GBP": {
+        "nominal": "1",
+        "name": "фунт стерлингов",
     },
-    {
-        'code': 'AMD',
-        'nominal': '100',
-        'name': 'армянских драмов',
+    "AMD": {
+        "nominal": "100",
+        "name": "армянских драмов",
     },
-    {
-        'code': 'BYN',
-        'nominal': '1',
-        'name': 'белорусский рубль',
+    "BYN": {
+        "nominal": "1",
+        "name": "белорусский рубль",
     },
-    {
-        'code': 'BGN',
-        'nominal': '1',
-        'name': 'болгарский лев',
+    "BGN": {
+        "nominal": "1",
+        "name": "болгарский лев",
     },
-    {
-        'code': 'BRL',
-        'nominal': '1',
-        'name': 'бразильский реал',
+    "BRL": {
+        "nominal": "1",
+        "name": "бразильский реал",
     },
-    {
-        'code': 'HUF',
-        'nominal': '100',
-        'name': 'венгерских форинтов',
+    "HUF": {
+        "nominal": "100",
+        "name": "венгерских форинтов",
     },
-    {
-        'code': 'HKD',
-        'nominal': '1',
-        'name': 'гонконгский доллар',
+    "HKD": {
+        "nominal": "1",
+        "name": "гонконгский доллар",
     },
-    {
-        'code': 'DKK',
-        'nominal': '1',
-        'name': 'датская крона',
+    "DKK": {
+        "nominal": "1",
+        "name": "датская крона",
     },
-    {
-        'code': 'USD',
-        'nominal': '1',
-        'name': 'доллар США',
+    "USD": {
+        "nominal": "1",
+        "name": "доллар США",
     },
-    {
-        'code': 'EUR',
-        'nominal': '1',
-        'name': 'евро',
+    "EUR": {
+        "nominal": "1",
+        "name": "евро",
     },
-    {
-        'code': 'INR',
-        'nominal': '10',
-        'name': 'индийских рупий',
+    "INR": {
+        "nominal": "10",
+        "name": "индийских рупий",
     },
-    {
-        'code': 'KZT',
-        'nominal': '100',
-        'name': 'казахстанских тенге',
+    "KZT": {
+        "nominal": "100",
+        "name": "казахстанских тенге",
     },
-    {
-        'code': 'CAD',
-        'nominal': '1',
-        'name': 'канадский доллар',
+    "CAD": {
+        "nominal": "1",
+        "name": "канадский доллар",
     },
-    {
-        'code': 'KGS',
-        'nominal': '100',
-        'name': 'киргизских сомов',
+    "KGS": {
+        "nominal": "100",
+        "name": "киргизских сомов",
     },
-    {
-        'code': 'CNY',
-        'nominal': '1',
-        'name': 'китайский юань',
+    "CNY": {
+        "nominal": "1",
+        "name": "китайский юань",
     },
-    {
-        'code': 'MDL',
-        'nominal': '10',
-        'name': 'молдавских леев',
+    "MDL": {
+        "nominal": "10",
+        "name": "молдавских леев",
     },
-    {
-        'code': 'NOK',
-        'nominal': '10',
-        'name': 'норвежских крон',
+    "NOK": {
+        "nominal": "10",
+        "name": "норвежских крон",
     },
-    {
-        'code': 'PLN',
-        'nominal': '1',
-        'name': 'польский злотый',
+    "PLN": {
+        "nominal": "1",
+        "name": "польский злотый",
     },
-    {
-        'code': 'RON',
-        'nominal': '1',
-        'name': 'румынский лей',
+    "RON": {
+        "nominal": "1",
+        "name": "румынский лей",
     },
-    {
-        'code': 'SGD',
-        'nominal': '1',
-        'name': 'сингапурский доллар',
+    "SGD": {
+        "nominal": "1",
+        "name": "сингапурский доллар",
     },
-    {
-        'code': 'TJS',
-        'nominal': '10',
-        'name': 'таджикских сомони',
+    "TJS": {
+        "nominal": "10",
+        "name": "таджикских сомони",
     },
-    {
-        'code': 'TRY',
-        'nominal': '10',
-        'name': 'турецких лир',
+    "TRY": {
+        "nominal": "10",
+        "name": "турецких лир",
     },
-    {
-        'code': 'TMT',
-        'nominal': '1',
-        'name': 'новый туркменский манат',
+    "TMT": {
+        "nominal": "1",
+        "name": "новый туркменский манат",
     },
-    {
-        'code': 'UZS',
-        'nominal': '10000',
-        'name': 'узбекских сумов',
+    "UZS": {
+        "nominal": "10000",
+        "name": "узбекских сумов",
     },
-    {
-        'code': 'UAH',
-        'nominal': '10',
-        'name': 'украинских гривен',
+    "UAH": {
+        "nominal": "10",
+        "name": "украинских гривен",
     },
-    {
-        'code': 'CZK',
-        'nominal': '10',
-        'name': 'чешских крон',
+    "CZK": {
+        "nominal": "10",
+        "name": "чешских крон",
     },
-    {
-        'code': 'SEK',
-        'nominal': '10',
-        'name': 'шведских крон',
+    "SEK": {
+        "nominal": "10",
+        "name": "шведских крон",
     },
-    {
-        'code': 'CHF',
-        'nominal': '1',
-        'name': 'швейцарский франк',
+    "CHF": {
+        "nominal": "1",
+        "name": "швейцарский франк",
     },
-    {
-        'code': 'ZAR',
-        'nominal': '10',
-        'name': 'южноафриканских рэндов',
+    "ZAR": {
+        "nominal": "10",
+        "name": "южноафриканских рэндов",
     },
-    {
-        'code': 'KRW',
-        'nominal': '1000',
-        'name': 'вон Республики Корея',
+    "KRW": {
+        "nominal": "1000",
+        "name": "вон Республики Корея",
     },
-    {
-        'code': 'JPY',
-        'nominal': '100',
-        'name': 'японских иен',
+    "JPY": {
+        "nominal": "100",
+        "name": "японских иен",
     },
-]
+}
